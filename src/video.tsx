d2fbbfa05be3b19d4104e0be0e501c7a12135d33
Rodrigo Pombo <pombopombopombo@gmail.com>
2021-05-30 23:37:10 -0300

    Init

---
+++ src/video.tsx
@@ -0,0 +1,28 @@
+import {useCurrentFrame, useVideoConfig} from 'remotion';
+import {RepoHeader} from './repo-header';
+
+export const Video: React.FC<{
+	repoOrg: string;
+	repoName: string;
+	starCount: number;
+}> = ({repoOrg, repoName, starCount}) => {
+	const frame = useCurrentFrame();
+	const videoConfig = useVideoConfig();
+
+	return (
+		<div style={{flex: 1, backgroundColor: '#f6f8fa'}}>
+			<RepoHeader stars={frame} org={repoOrg} name={repoName} />
+			<div
+				style={{
+					margin: 24,
+					background: 'white',
+					border: '1px solid #e1e4e8',
+					borderRadius: 6,
+					padding: 18,
+				}}
+			>
+				Hello
+			</div>
+		</div>
+	);
+};
