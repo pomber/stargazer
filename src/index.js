b5f8e9f5cd842fdbd2ce330686e217c24a9992b0
Rodrigo Pombo <pombopombopombo@gmail.com>
2021-05-30 23:54:32 -0300

    TS to JS

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { Composition, registerRoot } from "remotion";
+import { Video } from "./video";
+
+function RemotionVideo() {
+  return (
+    <Composition
+      id="main"
+      component={Video}
+      durationInFrames={60 * 5}
+      fps={30}
+      width={1280 / 2.5}
+      height={720 / 2.5}
+      defaultProps={{
+        repoOrg: "code-hike",
+        repoName: "codehike",
+        starCount: 100,
+      }}
+    />
+  );
+}
+
+registerRoot(RemotionVideo);
