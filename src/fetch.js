f3620fbe7b3ccde1a5e6655a841c64d73a29826a
Rodrigo Pombo <pombopombopombo@gmail.com>
2021-06-11 15:16:25 -0500

    Fetch pages

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,27 @@
-export function fetchStargazers(repoOrg, repoName, starCount) {
+export async function fetchStargazers(repoOrg, repoName, starCount) {
+  let starsLeft = starCount;
+  let cursor = null;
+  let allStargazers = [];
+
+  while (starsLeft > 0) {
+    const count = Math.min(starsLeft, 100);
+    const [newCursor, page] = await fetchPage(repoOrg, repoName, count, cursor);
+    allStargazers = [...allStargazers, ...page];
+    cursor = newCursor;
+    if (page.length < count) {
+      starsLeft = 0;
+    } else {
+      starsLeft = starsLeft - page.length;
+    }
+  }
+
+  return allStargazers;
+}
+
+function fetchPage(repoOrg, repoName, count, cursor) {
   const query = `{
     repository(owner: "${repoOrg}", name: "${repoName}") {
-      stargazers(first: 100) {
+      stargazers(first: ${count}${cursor ? `, after: "${cursor}"` : ""}) {
         edges {
           starredAt
           node {
@@ -14,7 +34,6 @@ export function fetchStargazers(repoOrg, repoName, starCount) {
       }
     }
   }`;
-
   return fetch("https://api.github.com/graphql", {
     method: "POST",
     headers: {
@@ -33,11 +52,13 @@ export function fetchStargazers(repoOrg, repoName, starCount) {
     })
     .then((res) => {
       const { edges } = res.data.repository.stargazers;
-      return edges.map((edge) => ({
+      const lastCursor = edges[edges.length - 1].cursor;
+      const page = edges.map((edge) => ({
         avatarUrl: edge.node.avatarUrl,
         date: edge.starredAt,
         name: edge.node.name || edge.node.login,
       }));
+      return [lastCursor, page];
     })
     .catch((e) => console.error(e));
 }
