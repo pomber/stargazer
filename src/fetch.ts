dcb82058e91d7a9c50ee1bf4f29aa65e608c64c7
Jonny Burger <jonathanburger11@gmail.com>
2023-06-20 09:49:27 +0200

    typesafe API calls

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,45 +1,45 @@
 export type Stargazer = {
-  avatarUrl: string;
-  name: string;
-  date: string;
+	avatarUrl: string;
+	name: string;
+	date: string;
 };
 
 export async function fetchStargazers(
-  repoOrg: string,
-  repoName: string,
-  starCount: number
+	repoOrg: string,
+	repoName: string,
+	starCount: number
 ) {
-  let starsLeft = starCount;
-  let cursor = null;
-  let allStargazers: Stargazer[] = [];
+	let starsLeft = starCount;
+	let cursor = null;
+	let allStargazers: Stargazer[] = [];
 
-  while (starsLeft > 0) {
-    const count = Math.min(starsLeft, 100);
-    const result = await fetchPage(repoOrg, repoName, count, cursor);
-    if (!result) return result;
+	while (starsLeft > 0) {
+		const count = Math.min(starsLeft, 100);
+		const result = await fetchPage(repoOrg, repoName, count, cursor);
+		if (!result) return result;
 
-    const [newCursor, page] = result;
-    allStargazers = [...allStargazers, ...page];
-    cursor = newCursor;
-    if (page.length < count) {
-      starsLeft = 0;
-    } else {
-      starsLeft = starsLeft - page.length;
-    }
-  }
+		const [newCursor, page] = result;
+		allStargazers = [...allStargazers, ...page];
+		cursor = newCursor;
+		if (page.length < count) {
+			starsLeft = 0;
+		} else {
+			starsLeft -= page.length;
+		}
+	}
 
-  return allStargazers;
+	return allStargazers;
 }
 
 async function fetchPage(
-  repoOrg: string,
-  repoName: string,
-  count: number,
-  cursor: number | null
-): Promise<[number, Stargazer[]]> {
-  const query = `{
+	repoOrg: string,
+	repoName: string,
+	count: number,
+	cursor: string | null
+): Promise<[string, Stargazer[]]> {
+	const query = `{
     repository(owner: "${repoOrg}", name: "${repoName}") {
-      stargazers(first: ${count}${cursor ? `, after: "${cursor}"` : ""}) {
+      stargazers(first: ${count}${cursor ? `, after: "${cursor}"` : ''}) {
         edges {
           starredAt
           node {
@@ -52,34 +52,54 @@ async function fetchPage(
       }
     }
   }`;
-  if (!process.env.REMOTION_GITHUB_TOKEN) {
-    throw new TypeError(
-      "You need to set a REMOTION_GITHUB_TOKEN environment variable"
-    );
-  }
+	if (!process.env.REMOTION_GITHUB_TOKEN) {
+		throw new TypeError(
+			'You need to set a REMOTION_GITHUB_TOKEN environment variable'
+		);
+	}
 
-  const res = await fetch("https://api.github.com/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      authorization: "token " + process.env.REMOTION_GITHUB_TOKEN,
-    },
-    body: JSON.stringify({ query }),
-  });
-  if (!res.ok) {
-    return res.text().then((textResponse) => {
-      throw Error(`HTTP ${res.status} ${res.statusText}: ${textResponse}`);
-    });
-  }
-  const res_1 = await res.json();
-  const { edges } = res_1.data.repository.stargazers;
-  const lastCursor: number = edges[edges.length - 1].cursor;
-  const page: Stargazer[] = edges.map((edge: any): Stargazer => {
-    return {
-      avatarUrl: edge.node.avatarUrl,
-      date: edge.starredAt,
-      name: edge.node.name || edge.node.login,
-    };
-  });
-  return [lastCursor, page];
+	const res = await fetch('https://api.github.com/graphql', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			authorization: 'token ' + process.env.REMOTION_GITHUB_TOKEN,
+		},
+		body: JSON.stringify({query}),
+	});
+	if (!res.ok) {
+		return res.text().then((textResponse) => {
+			throw Error(`HTTP ${res.status} ${res.statusText}: ${textResponse}`);
+		});
+	}
+	const res1 = (await res.json()) as GitHubApiResponse;
+	const {edges} = res1.data.repository.stargazers;
+	const lastCursor = edges[edges.length - 1].cursor;
+	const page: Stargazer[] = edges.map((edge): Stargazer => {
+		return {
+			avatarUrl: edge.node.avatarUrl,
+			date: edge.starredAt,
+			name: edge.node.name || edge.node.login,
+		};
+	});
+	return [lastCursor, page];
+}
+
+interface GitHubApiResponse {
+	data: {
+		repository: {
+			stargazers: {
+				edges: Edge[];
+			};
+		};
+	};
+}
+
+interface Edge {
+	starredAt: string;
+	node: {
+		avatarUrl: string;
+		name?: string;
+		login: string;
+	};
+	cursor: string;
 }
