ba2d13fd63375294619e06f9eb4852a3e569d783
Jonny Burger <jonathanburger11@gmail.com>
2023-06-20 10:11:27 +0200

    log stars being fetched

--- src/fetch.ts
+++ src/fetch.ts
@@ -13,13 +13,14 @@ export async function fetchStargazers(
 	let cursor = null;
 	let allStargazers: Stargazer[] = [];
 
+	console.log('Fetching stars...');
 	while (starsLeft > 0) {
 		const count = Math.min(starsLeft, 100);
-		const result = await fetchPage(repoOrg, repoName, count, cursor);
-		if (!result) return result;
+		const result = await fetchPage({repoOrg, repoName, count, cursor});
 
-		const [newCursor, page] = result;
+		const {cursor: newCursor, page} = result;
 		allStargazers = [...allStargazers, ...page];
+		console.log('Fetched ', allStargazers.length, ' stars');
 		cursor = newCursor;
 		if (page.length < count) {
 			starsLeft = 0;
@@ -31,27 +32,32 @@ export async function fetchStargazers(
 	return allStargazers;
 }
 
-async function fetchPage(
-	repoOrg: string,
-	repoName: string,
-	count: number,
-	cursor: string | null
-): Promise<[string, Stargazer[]]> {
+async function fetchPage({
+	repoOrg,
+	repoName,
+	count,
+	cursor,
+}: {
+	repoOrg: string;
+	repoName: string;
+	count: number;
+	cursor: string | null;
+}): Promise<{cursor: string; page: Stargazer[]}> {
 	const query = `{
-    repository(owner: "${repoOrg}", name: "${repoName}") {
-      stargazers(first: ${count}${cursor ? `, after: "${cursor}"` : ''}) {
-        edges {
-          starredAt
-          node {
-            avatarUrl
-            name
-            login
-          }
-          cursor
-        }
-      }
-    }
-  }`;
+		repository(owner: "${repoOrg}", name: "${repoName}") {
+			stargazers(first: ${count}${cursor ? `, after: "${cursor}"` : ''}) {
+				edges {
+					starredAt
+					node {
+						avatarUrl
+						name
+						login
+					}
+					cursor
+				}
+			}
+		}
+	}`;
 
 	if (!process.env.REMOTION_GITHUB_TOKEN) {
 		throw new TypeError(
@@ -67,6 +73,7 @@ async function fetchPage(
 		},
 		body: JSON.stringify({query}),
 	});
+
 	if (!res.ok) {
 		const textResponse = await res.text();
 		throw Error(`HTTP ${res.status} ${res.statusText}: ${textResponse}`);
@@ -75,14 +82,14 @@ async function fetchPage(
 	const json = (await res.json()) as GitHubApiResponse;
 	const {edges} = json.data.repository.stargazers;
 	const lastCursor = edges[edges.length - 1].cursor;
-	const page: Stargazer[] = edges.map((edge): Stargazer => {
+	const page: Stargazer[] = edges.map((edge) => {
 		return {
 			avatarUrl: edge.node.avatarUrl,
 			date: edge.starredAt,
 			name: edge.node.name || edge.node.login,
 		};
 	});
-	return [lastCursor, page];
+	return {cursor: lastCursor, page};
 }
 
 interface GitHubApiResponse {
