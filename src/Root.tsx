56825b4ec5357cefbe1f29aefcf14817ed4fdcdb
Jonny Burger <jonathanburger11@gmail.com>
2024-01-26 10:08:21 +0100

    remotion naming convention to make tests pass in our monorepo

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,12 @@
 import {useCallback} from 'react';
 import {CalculateMetadataFunction, Composition} from 'remotion';
-import {Main, MainProps, mainSchema} from './Main';
 import {fetchStargazers} from './fetch/fetch-data';
+import {Main, MainProps, mainSchema} from './Main';
 import {waitForNoInput} from './wait-for-no-input';
 
 const FPS = 30;
 
-export function RemotionVideo() {
+export function RemotionRoot() {
 	const calculateMetadata: CalculateMetadataFunction<MainProps> = useCallback(
 		async ({props, abortSignal}) => {
 			await waitForNoInput(abortSignal, 500);
