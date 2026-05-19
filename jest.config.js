ad8b4ebbfd1000b9af8a0c9c22f223d5b00602fa
ToTheos-Dev <ToTheos-Dev 97577314+ToTheos-Dev@users.noreply.github.com>
2026-05-19 10:36:16 +1000

    test: add test configuration

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.js"
+  }
+};
